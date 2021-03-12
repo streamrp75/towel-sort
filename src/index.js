422b91bfad0efc7bbefc52153420e045d1e46c54
streamrp75 <79273733+streamrp75@users.noreply.github.com>
2021-03-12 19:20:36 +0300

    feat: add task code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == undefined || matrix == 0 ){
+    return []
+  }
+  let i=0
+  let A= []
+  for(i=0;i<matrix.length;i++)
+  {
+    if(matrix[i] == undefined || matrix[i] == 0 ){
+      A.push([])
+    }
+      if(i%2 == 0)
+      {
+        A=A.concat(matrix[i])
+      }
+      if(i%2 !=0 )
+      {
+        A=A.concat(matrix[i].reverse())
+      }
+    
+  }
+  return A
 }
